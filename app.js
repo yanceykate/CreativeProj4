46d70d0ed50f76ba471f27ac9ffb50c23b1ecde4
Kate Yancey <32001100+yanceykate@users.noreply.github.com>
2017-11-03 14:20:49 -0600

    Add files via upload
    Final

--- app.js
+++ app.js
@@ -1,60 +1,71 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = window.angular.module('app', [])
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+app.factory('pokemonFetcher', pokemonFetcher)
+app.controller('mainCtrl', mainCtrl)
 
-var app = express();
+function pokemonFetcher ($http) {
+  
+  var API_ROOT = 'pokemon'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+    tryit: function() {
+      var politics = "/politics";
+      return $http
+        .get(politics)
+        .then(function (resp) {
+          console.log("Get Worked");
+          console.log(resp.data);
+          return resp.data
+        })
+    }
+  }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+function mainCtrl ($scope, pokemonFetcher, $http) {
 
+  $scope.pokemon = []
+  $scope.poly = []
 
-module.exports = app;
+  pokemonFetcher.get()
+    .then(function (data) {
+      $scope.pokemon = data
+    })
+
+  pokemonFetcher.tryit()
+    .then(function (data) {
+      console.log("tryit");
+      console.log(data);
+	  $scope.poly = data;
+    });
+	
+$scope.addPoki = function() {
+	  var formData = {name:$scope.Name,avatarUrl:$scope.Url};
+	  console.log(formData);
+	  var pokiURL = 'pokemon';
+	  $http({
+		 url: pokiURL,
+		 method: "POST",
+		 data: formData
+	  }).success(function(data, status, headers, config) {
+		console.log("Post worked");
+	  }).error(function(data, status, headers, config) {
+		console.log("Post failed");
+	  });
+	}
+}
+function setCookie(key, value) {
+            var expires = new Date();
+            expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+        }
+ function getCookie(key) {
+            var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+            return keyValue ? keyValue[2] : null;
+        }
