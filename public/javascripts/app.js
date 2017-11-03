6a517b664a8d5ef407a87164812a7ab5af6d4edd
Kate Yancey <32001100+yanceykate@users.noreply.github.com>
2017-11-03 14:24:58 -0600

    Final

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -60,3 +60,12 @@ $scope.addPoki = function() {
 	  });
 	}
 }
+function setCookie(key, value) {
+            var expires = new Date();
+            expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+        }
+ function getCookie(key) {
+            var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+            return keyValue ? keyValue[2] : null;
+        }
